7cd39f4aad73cb1cc26520ec0a696b7e15d3c739
XSasaX <140924362+XSasaX@users.noreply.github.com>
2025-05-31 00:38:34 +0200

    Delete dist directory

--- dist/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Karteikarte_Ver-nderbar/sw.js', { scope: '/Karteikarte_Ver-nderbar/' })})}
